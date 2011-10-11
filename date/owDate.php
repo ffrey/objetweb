caaecfba08532b6c383ae966662a6cf1b5f4297f
ffrey.web <ffrey.web@24f096bd-9e82-d8ed-94e9-3bb52fd9f2a6>
2011-10-11 09:04:15 +0000

    ajout class owDate (avec tests)

---
+++ date/owDate.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * static methods to act as helper functions for routine date processing
+ *
+ * @version 1.1  / 2009 03 17
+ * * added : 
+ * * * function stripSpaces()
+ * @throws Exception
+ */
+class owDate {
+
+    /**
+     *
+     * @param string $str : 'dd/mm/yyyy' / 'dd/mm/yyyy hh:mn:ss'
+     * @return bool
+     */
+    public static function isPassee($date) {
+        $db = false;
+        $origin = __CLASS__ . '::' . __FUNCTION__;
+		if ($db) { var_dump($origin, $date); }
+        if (preg_match('#^(\d{2})/(\d{2})/(\d{4})$#', $date, $p)) {
+		        if ($db) {
+            var_dump($origin, 'apres preg_match', $p);
+            // exit;
+        }
+            $formatUS = $p[3] . '-' . $p[2] . '-' . $p[1];
+        } else if (preg_match('#^(\d{2})/(\d{2})/(\d{4})\s{1}(\d{2}):(\d{2}):(\d{2})$#', $date, $p)) {
+            $formatUS = $p[3] . '-' . $p[2] . '-' . $p[1] . ' ' . $p[4] . ':' . $p[5] . ':' . $p[6];
+        } else {
+            throw new Exception($date . ' n\'est pas au format "dd/mm/yyyy" ou "dd/mm/yyyy hh:mn:ss"');
+        }
+        if ($db) {
+            var_dump($origin, 'apres preg_match', $p);
+            // exit;
+        }
+        $now = time();
+        $dateTime = strtotime($formatUS);
+        $ret = ($dateTime < $now);
+        if ($db) {
+            var_dump($origin, $ret);
+            print '<h3>' . $date . ' < NOW (' . strftime('%d/%m/%Y %H:%M:%S', $now) . ') ?</h3>';
+            print '<h3>' . $dateTime . ' < ' . $now . ' ?</h3>';
+        }
+        return $ret;
+    }
+
+}
---
+++ date/owDateTest.php
@@ -0,0 +1,53 @@
+<?php
+// phpunit C:\wamp\lib\ow\date\owDateTest.php
+
+//require_once 'PHPUnit/Extensions/SeleniumTestCase.php';
+require_once 'owDate.php';
+
+/**
+ * @see http://www.php.net/manual/fr/function.is-numeric.php
+ */
+class owDateTest extends PHPUnit_Framework_TestCase
+{
+  public function testIsPassee() 
+  {
+	$db = false;
+	date_default_timezone_set('Europe/Paris');
+    $iNowPlusUneSeconde  = time() + 1;
+	$iNowMoinsUneSeconde = time() - 1;
+	list($sNowDate, $sNowHeure) = $this->formatIntoFullDate($iNowPlusUneSeconde);
+	$sNowPlusUneSeconde = $sNowDate . ' ' . $sNowHeure;
+	list($sNowDate, $sNowHeure) = $this->formatIntoFullDate($iNowMoinsUneSeconde);
+	$sNowMoinsUneSeconde = $sNowDate . ' ' . $sNowHeure;
+	if ($db) { 
+	    var_dump('TIME ZONE',  date_default_timezone_get() );
+		var_dump('date full', $sNowFull); exit;
+	}
+	$tests = array(
+		/* */
+		array('10/01/2020', false,),
+		array('10/05/2010', true),
+		array($sNowDate, true), // date auj sans heure est consideree egale a 00:00:00	
+		array($sNowMoinsUneSeconde, true),
+		array($sNowPlusUneSeconde,   false),
+	);
+	foreach ($tests AS $t) {
+		$date = $t[0];
+		$expect = $t[1];
+		$got = owDate::isPassee($date);
+		$this->assertEquals($expect, $got,
+			printf('isPassee(%s) should be %s'."\n", $date, $expect) 
+		);
+	}
+  }
+
+  private function formatIntoFullDate($time)
+  {
+	$i = getDate($time);
+	$sNow = implode(array(str_pad($i['mday'], 2, "0", STR_PAD_LEFT), str_pad($i['mon'], 2, "0", STR_PAD_LEFT), $i['year']), '/');
+	$sNowHeure = implode(array(str_pad($i['hours'], 2, "0", STR_PAD_LEFT), str_pad($i['minutes'], 2, "0", STR_PAD_LEFT), str_pad($i['seconds'], 2, "0", STR_PAD_LEFT) ), ':');
+	
+	return array($sNow, $sNowHeure);
+  }
+  
+}
