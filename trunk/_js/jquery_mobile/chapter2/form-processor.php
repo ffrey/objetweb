4fd37faa1f7d456cfdf8782bed40710baaa38c1f
ffrey.web <ffrey.web@24f096bd-9e82-d8ed-94e9-3bb52fd9f2a6>
2011-11-27 21:33:32 +0000

    pdf o'reilly jquery mobile

---
+++ trunk/_js/jquery_mobile/chapter2/form-processor.php
@@ -0,0 +1,22 @@
+<?php
+
+?>
+<html>
+<head>
+
+</head>
+<body>
+ <section id="page-confirmation" data-role="page">
+      <header data-role="header">
+        <h1>confirmation</h1>
+      </header>
+      <div data-role="content" class="content">
+        <p>Form confirmed !</p>
+        
+      </div>
+      <footer data-role="footer">
+        <h1>@ljkjlj</h1>
+      </footer>
+    </section>
+</body>
+</html>
