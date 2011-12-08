3e3e094e661c14bc1adefa5022ef29dd3a1ac67d
ffrey.web <ffrey.web@24f096bd-9e82-d8ed-94e9-3bb52fd9f2a6>
2011-12-08 16:45:50 +0000

    amelioration owDate::isPassee() (plus de separateurs autorises, etc.)

--- date/owDate.php
+++ date/owDate.php
@@ -14,19 +14,23 @@ class owDate
      * ! la date du jour sans heure est consideree comme PASSEE !!!
 	 * 
      * @param string $str : 'dd/mm/yyyy' / 'dd/mm/yyyy hh:mn:ss'
+	 *              separateurs acceptes : - / espace
      * @return bool
      */
     public static function isPassee($date) {
         $db = false;
         $origin = __CLASS__ . '::' . __FUNCTION__;
-		if ($db) { var_dump($origin, $date); }
-        if (preg_match('#^(\d{2})/(\d{2})/(\d{4})$#', $date, $p)) {
+		if ($db) { var_dump($origin, $date, 'format date', $format_date, 'format date+heure', $format_date_heure); }
+		$sep = '[/\-\s]{1,1}'; // separateurs acceptes pour la date
+		$format_date       = '(\d{2})'.$sep.'(\d{2})'.$sep.'(\d{4})';
+		$format_date_heure = $format_date .'\s{1}(\d{2}):(\d{2}):(\d{2})'; 
+        if (preg_match('#^'.$format_date.'$#', $date, $p)) {
 		    if ($db) {
 				var_dump($origin, 'apres preg_match', $p);
 				// exit;
 			}
             $formatUS = $p[3] . '-' . $p[2] . '-' . $p[1];
-        } else if (preg_match('#^(\d{2})/(\d{2})/(\d{4})\s{1}(\d{2}):(\d{2}):(\d{2})$#', $date, $p)) {
+        } else if (preg_match('#^'.$format_date_heure.'$#', $date, $p)) {
             $formatUS = $p[3] . '-' . $p[2] . '-' . $p[1] . ' ' . $p[4] . ':' . $p[5] . ':' . $p[6];
         } else {
             throw new Exception($date . ' n\'est pas au format "dd/mm/yyyy" ou "dd/mm/yyyy hh:mn:ss"');
