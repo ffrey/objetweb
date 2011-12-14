55a23aa6ef9b33eed3d4102b6246f8114cabc2fb
ffrey.web <ffrey.web@24f096bd-9e82-d8ed-94e9-3bb52fd9f2a6>
2011-12-14 17:12:39 +0000

    studying BASICS : to p.18

--- trunk/_etude_php5/_basics/operatorsTest.php
+++ trunk/_etude_php5/_basics/operatorsTest.php
@@ -2,18 +2,70 @@
 // phpunit C:\wamp\lib\ow\_etude_php5\_basics\operatorsTest.php
 require_once dirname(__FILE__).'/../_bootstrap.php';
 
-class owDateTest extends PHPUnit_Framework_TestCase
+class operatorsTest extends PHPUnit_Framework_TestCase
 {
 	public function testEither()
 	{
-		step('operator EITHER-OR');
+		step('Bitwise Operators : & | ^');
+		cmd('return integer : 1 or 0');
 		$tests = array(
 		array(1 ^ 0, 1),
 		array(1 ^ 1, 0),
+
 		);
 		foreach ($tests AS $t) {
 			
 			$this->assertEquals($t[0], $t[1]);
 		}
+	}
+	
+	public function testArrayOperators()
+	{
+		step('array ops');
+		cmd('+ union');
+		$a1 = array('a' => 'tab1', 'b' => 'tab1');
+		$a2 = array('a' => 'tab2', 'c' => 'tab2');
+		$expect_union = array(
+		'a' => 'tab1',
+		'b' => 'tab1',
+		'c' => 'tab2',
+		);
+		$expect_merge = $expect_union;
+		$got = $a1 + $a2;
+		$this->assertEquals($got, $expect_union,
+		'union : left-hand array takes precedence');
+		$expect_merge['a'] = 'tab2';
+		$got = array_merge($a1, $a2);
+		$this->assertEquals($got, $expect_merge,
+		'merge : right-hand array takes precedence');
+		
+	}
+	
+	public function testLogicalOps()
+	{
+		step('Logical Operators');
+		cmd('return boolean !');
+		$tests = array(
+			array(1 XOR 1, false),
+		);
+		foreach ($tests AS $t) {
+			$this->assertEquals($t[0], $t[1]);
+		}
+	}
+	
+	public function testExecutionOperators()
+	{
+		step('Execution Operators');
+		cmd('Use backticks `` to execute the content as shell command');
+		$expect = shell_exec('ls -ls');
+		$got = `ls -ls`;
+		$this->assertEquals($got, $expect);
+	}
+	
+	public function testConstructs()
+	{
+		step('Constructs');
+		cmd('die & exit, eval are functions, hence use parentheses if needed !');
+		cmd('return is a control structure, hence no need to use parentheses... !');
 	}
 }
