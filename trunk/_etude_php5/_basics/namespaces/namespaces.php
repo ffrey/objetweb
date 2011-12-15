2908dc332bea082d4e20187fe4b0cd31ef1258b0
ffrey.web <ffrey.web@24f096bd-9e82-d8ed-94e9-3bb52fd9f2a6>
2011-12-15 17:12:18 +0000

    avancement dans namespaces

--- trunk/_etude_php5/_basics/namespaces/namespaces.php
+++ trunk/_etude_php5/_basics/namespaces/namespaces.php
@@ -1,10 +1,12 @@
 <?php
+// C:\wamp\lib\ow\_etude_php5\_basics\namespaces>php535 namespaces.php
 namespace my\name; // see "Defining Namespaces" section
 
 class MyClass {}
 function myfunction() {}
 const MYCONST = 'hello';
-
+define('TRADI_CONST', 'tradi');
+define('my\name\NS_DEFINED', 'definie dans ns avec define()');
 $a = new MyClass;
 $c = new \my\name\MyClass; // see "Global Space" section
 
@@ -14,4 +16,13 @@ $a = strlen('hi'); // see "Using namespaces: fallback to global
 $d = namespace\MYCONST; // see "namespace operator and __NAMESPACE__
                         // constant" section
 $d = __NAMESPACE__ . '\MYCONST';
-echo constant($d); // see "Namespaces and dynamic language features" section
+printf('constante definie avec const dans ns %s : %s'.PHP_EOL, __NAMESPACE__, constant($d) ); // see "Namespaces and dynamic language features" section
+if (!defined('\my\name\TRADI_CONST') ) {
+	print 'define() ne definit des constantes QUE dans le scope global !!!'.PHP_EOL;
+}
+printf('constante definie avec define() : %s'.PHP_EOL        ,  TRADI_CONST); 
+printf('\my\name\MYCONST existe ? => %s'.PHP_EOL, defined('\my\name\MYCONST')?'true':'false' );
+
+if (defined('my\name\NS_DEFINED') ) {
+	printf('on pt aussi utiliser define() a la place de const pour creer une constante dans un ns : %s', \my\name\NS_DEFINED);
+}
