a0ef40d1310725a5e1bdff136f064e8060737570
ffrey.web <ffrey.web@24f096bd-9e82-d8ed-94e9-3bb52fd9f2a6>
2012-01-24 18:02:59 +0000

    avanct : xml

--- trunk/_etude_php5/2_data_types_and_formats/xml/simple_xpath.php
+++ trunk/_etude_php5/2_data_types_and_formats/xml/simple_xpath.php
@@ -1,7 +1,33 @@
 <?php
-
+/**
+ * 2012 01 24 : comment afficher avec le bon encoding ?
+ *
+ * use : http://simplehtmldom.sourceforge.net/ <= http://davidwalsh.name/php-notifications
+ *       http://framework.zend.com/manual/en/zend.dom.query.html
+ */
+// C:\wamp\lib\ow\_etude_php5\2_data_types_and_formats\xml\php53 simple_xpath.php
 # recup html d'une page web
+// create a new cURL resource
+$ch = curl_init();
 
+// set URL and other appropriate options
+curl_setopt($ch, CURLOPT_URL, "http://www.msf.fr/rss.xml");
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
+// grab URL and pass it to the browser
+$page = curl_exec($ch);
+
+// close cURL resource, and free up system resources
+curl_close($ch);
+
+// var_dump($page);
 # recup par xpath de simple xml une partie de la page
-
+$xml = simplexml_load_string($page);
+$path = "channel/item/title";
+// div/div[@id='wrapper']/div[2]/div[1]/div/div/div/div/div/div[1]/ul[@id='slider1']/li[3]/h2/a 
+$result = $xml->xpath($path);
+// var_dump($result);
+while(list( , $node) = each($result)) {
+    echo 'content ',$node,"\n";
+}
