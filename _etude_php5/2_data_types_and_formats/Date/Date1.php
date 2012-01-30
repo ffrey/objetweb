59182a3eff742fa4129dc6462e2767e358eabc60
ffrey.web <ffrey.web@24f096bd-9e82-d8ed-94e9-3bb52fd9f2a6>
2012-01-30 13:54:38 +0000

    cert : - fin data types

---
+++ _etude_php5/2_data_types_and_formats/Date/Date1.php
@@ -0,0 +1,5 @@
+<?php
+// php53 C:\wamp\lib\ow\_etude_php5\2_data_types_and_formats\Date\Date1.php
+$timezone_abbr = DateTimeZone::listIdentifiers(DateTimeZone::EUROPE, 'FR');
+
+var_dump($timezone_abbr);
