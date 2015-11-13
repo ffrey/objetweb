d5cade7d88f8f6f12e690d470cc84df720f036ac
unknown <ffreysse@FRYSDSSC0645.global.publicisgroupe.net>
2015-11-13 15:44:02 +0100

    owArray

--- owArray.php
+++ owArray.php
@@ -1,142 +1,200 @@
 <?php
+
 /**
- * static methods to act as helper functions for routine array processing
- *
- * @throws Exception
- *
+ * @author : ffrey
  */
-class owArray
+class owArray 
 {
-	/**
-	 * @param $newKeys array : [<old key>] => [<new key>]
-	 */
-	public static function translateKeys(array $withKeysToBeTranslated, array $newKeys)
-	{
-		$ret = array();
-		// $keys = array_flip($newKeys);
-		foreach ($withKeysToBeTranslated AS $key => $val) {
-			if (!key_exists($key, $newKeys ) ) {
-				$ret[$key] = $val; continue;
-	  }
-	  $ret[$newKeys[$key] ] = $val;
-		}
-		return $ret;
-	}
-	public static function notEmpty()
-	{
-		// use array_filter(array <$a>) without a callback !
-	}
-
-	/**
-	 * split $A into $slices sub arrays
-	 * @return array : 'key' => <slice> !
-	 * @todo manage uneven lengths !!! / @see if enhance poss with array_chunk() !
-	 */
-	public static function _split_into($A, $slices) {
-		$A_ret = array ();
-
-		$size = count ( $A );
-		if ($size < $slices) {
-			$slices = $size;
-		}
-		$sizeOfSlice = $size / $slices;
-		$slice = floor ( $sizeOfSlice );
-		$offset = 0;
-		$surplus = 0;
-		if ($sizeOfSlice != $slice)
-		$surplus = ($sizeOfSlice - $slice) * $slice;
-		for($i = 1; $i <= $slices; $i ++) {
-			$s = $slice;
-			if (0 < $surplus) {
-				$surplus --;
-				$s = $slice + 1; // if uneven => odd values added to first array(s)
+    /**
+     * @author : anonymous on http://www.php.net/manual/fr/function.array-unshift.php
+     * @param type $arr
+     * @param type $key
+     * @param
+     */
+    static public function array_unshift_assoc($arr, $key, $val)
+    {
+        $db = false; $origin = __CLASS__.'::'.__FUNCTION__;
+        if ($db) {
+            var_dump($origin, 'got', $arr);
+        }
+        $arr = array_reverse($arr, true);
+        $arr[$key] = $val;
+        if ($db) {
+            var_dump($origin, 'new', $arr);
+        }
+        $ret = array_reverse($arr, true);
+        if ($db) {
+            var_dump($origin, 'return', $ret);
+        }
+        return $ret;
+    }
+    
+	static public function removeFromArray(array $aKeyValue, array $arrayToFilter)
+		{
+			$db = false;
+			$origin = __CLASS__ . '::' . __FUNCTION__;
+						if ($db ){
+				var_dump($origin
+					, 'looking for ', print_r($aKeyValue, true) );
 			}
-			$A_ret [] = array_slice ( $A, $offset, $s );
-			$offset += $s;
-		}
-		return $A_ret;
-	}
-
-	/**
-	 * unsets all $keys existing in $A
-	 * @return array $A
-	 */
-	public static function multiUnset(array $A, array $keys) {
-		foreach ($keys AS $k) {
-			if (array_key_exists($k, $A) ) {
-				unset($A[$k]);
+			$arrayFiltered = $arrayToFilter;
+			foreach ($arrayToFilter AS $k => $l) {
+				if (!is_array($l) ) {
+					continue;
+				}
+				$iRemove = self::isArrayContained($aKeyValue, $l);
+				if ($iRemove) {
+					if ($db) {
+						var_dump($origin
+							, 'REMOVE', $arrayFiltered[$k]
+							);
+					}
+					unset($arrayFiltered[$k]);
+				}
 			}
-		}
-		return $A;
-	}
-
-	/*
-	 * Flattening a multi-dimensional array into a
-	 * single-dimensional one. The resulting keys are a
-	 * string-separated list of the original keys:
-	 *
-	 * a[x][y][z] becomes a[implode(sep, array(x,y,z))]
-	 * @author : Carsten Milkau (http://php.net/manual/en/function.array-values.php)
-	 */
-
-	public static function flatten_sep($sep, $array) {
-		$result = array();
-		$stack = array();
-		array_push($stack, array("", $array));
-
-		while (count($stack) > 0) {
-			list($prefix, $array) = array_pop($stack);
-
-			foreach ($array as $key => $value) {
-		  $new_key = $prefix . strval($key);
-
-		  if (is_array($value))
-		  array_push($stack, array($new_key . $sep, $value));
-		  else
-		  $result[$new_key] = $value;
-			}
-		}
-
-		return $result;
-	}
-
-	/**
-	 * adds keys+vals from $b onto $a
-	 * @param unknown_type $a
-	 * @param unknown_type $b
-	 */
-	public static function array_merge_with_numeric_keys(array $a, array $b)
-	{
-		foreach ($b AS $k => $v) {
-			$a[$k] = $v;
-		}
-		return $a;
-	}
-
-	/**
-	 * renvoie vide ou $default_val si $var_name_exists n'existe pas dans $tab ou est vide / la valeur sinon
-	 * @param $var_name_exists
-	 * @param $tab
-	 * @param $default_val
-	 */
-	public static function g_if ($var_name_exists, array $tab, $default_val = '' )
-	{
-		$db = false;
-		$ret = $default_val;
-		do {
 			if ($db) {
-				echo 'existe ' . $var_name_exists . ' ? ';
+				var_dump($origin
+					, 'ret', $arrayFiltered
+					);
 			}
-			$vals = $tab;
-			if (array_key_exists($var_name_exists, $vals) ) {
-				if ($db) { echo ' YES : ' . print_r($vals, true); }
-				$ret = $vals[$var_name_exists];
-			} 
-			if (empty($ret) ) {
-				$ret = $default_val;
-			}
-		} while (false);
+			return array_values($arrayFiltered);
+		}
 
-		return $ret;
-	}
+	// /_removeFromArray()
+
+		static public function extractFromArray(array $aKeyValue, array $arrayToFilter)
+		{
+			$db = false;
+			$origin = __CLASS__ . '::' . __FUNCTION__;
+			$arrayFiltered = $arrayToFilter;
+			foreach ($arrayToFilter AS $k => $l) {
+				if (!is_array($l) ) {
+					continue;
+				}
+				$iRemove = self::isArrayContained($aKeyValue, $l);
+				if (!$iRemove) {
+					unset($arrayFiltered[$k]);
+				}
+			}
+			return array_values($arrayFiltered);
+		}
+
+		static public function isArrayContained(array $aKeyValues, array $arrayToSearch)
+		{
+			$db = false;
+			$origin = __CLASS__ . '::' . __FUNCTION__;
+			$ret = true;
+			foreach ($aKeyValues AS $key => $val) {
+				if ($db) {
+					var_dump($origin
+						, 'existe ' . $key . ' ?', array_key_exists($key, $arrayToSearch)
+						);
+				}
+				if (!array_key_exists($key, $arrayToSearch)) {
+					$ret = false;
+					continue;
+				}
+								if ($db) {
+					var_dump($origin
+						, $arrayToSearch[$key] . ' != ' . $val . ' ?'
+						);
+				}
+				if ($val != $arrayToSearch[$key]) {
+					$ret = false;
+					continue;
+				}
+				if ($db) {
+					var_dump($origin, 'FOUND', $key . ' = ' . $val, $ret);
+				}
+			}
+			return $ret;
+		}
+
+		static public function reindexLinesWithValueFromKey($keyToUse, array $arrayToReIndex) {
+			$db = false;
+			$origin = __CLASS__ . '::' . __FUNCTION__;
+			$ret = array();
+			foreach ($arrayToReIndex AS $a) {
+				if (!isset($a[$keyToUse]) ) {
+					throw new Exception('Unknown key : ' . $keyToUse);
+				}
+				$ret[$a[$keyToUse] ] = $a;
+			}
+			return $ret;
+		}
+
+		/**
+		 * @source https://github.com/ramsey/array_column/blob/master/src/array_column.php
+		 */
+		static function array_column($input = null, $columnKey = null, $indexKey = null)
+    {
+        // Using func_get_args() in order to check for proper number of
+        // parameters and trigger errors exactly as the built-in array_column()
+        // does in PHP 5.5.
+        $argc = func_num_args();
+        $params = func_get_args();
+        if ($argc < 2) {
+            trigger_error("array_column() expects at least 2 parameters, {$argc} given", E_USER_WARNING);
+            return null;
+        }
+        if (!is_array($params[0])) {
+            trigger_error(
+                'array_column() expects parameter 1 to be array, ' . gettype($params[0]) . ' given',
+                E_USER_WARNING
+            );
+            return null;
+        }
+        if (!is_int($params[1])
+            && !is_float($params[1])
+            && !is_string($params[1])
+            && $params[1] !== null
+            && !(is_object($params[1]) && method_exists($params[1], '__toString'))
+        ) {
+            trigger_error('array_column(): The column key should be either a string or an integer', E_USER_WARNING);
+            return false;
+        }
+        if (isset($params[2])
+            && !is_int($params[2])
+            && !is_float($params[2])
+            && !is_string($params[2])
+            && !(is_object($params[2]) && method_exists($params[2], '__toString'))
+        ) {
+            trigger_error('array_column(): The index key should be either a string or an integer', E_USER_WARNING);
+            return false;
+        }
+        $paramsInput = $params[0];
+        $paramsColumnKey = ($params[1] !== null) ? (string) $params[1] : null;
+        $paramsIndexKey = null;
+        if (isset($params[2])) {
+            if (is_float($params[2]) || is_int($params[2])) {
+                $paramsIndexKey = (int) $params[2];
+            } else {
+                $paramsIndexKey = (string) $params[2];
+            }
+        }
+        $resultArray = array();
+        foreach ($paramsInput as $row) {
+            $key = $value = null;
+            $keySet = $valueSet = false;
+            if ($paramsIndexKey !== null && array_key_exists($paramsIndexKey, $row)) {
+                $keySet = true;
+                $key = (string) $row[$paramsIndexKey];
+            }
+            if ($paramsColumnKey === null) {
+                $valueSet = true;
+                $value = $row;
+            } elseif (is_array($row) && array_key_exists($paramsColumnKey, $row)) {
+                $valueSet = true;
+                $value = $row[$paramsColumnKey];
+            }
+            if ($valueSet) {
+                if ($keySet) {
+                    $resultArray[$key] = $value;
+                } else {
+                    $resultArray[] = $value;
+                }
+            }
+        }
+        return $resultArray;
+    }
 }
